66113426020bda00d09bdd2d91c00c95c3ec8e12
Larissa Silva <larissa.silva@xing.com>
2024-03-26 21:31:32 +0100

    Add live grep for selected word

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("v", "<leader>f", function()
+  vim.api.nvim_input("y")
+  vim.api.nvim_input("<cmd> Telescope live_grep <CR>")
+  vim.api.nvim_input("<c-r>")
+  vim.api.nvim_input("0")
+end, { desc = "Telescope live grep for selection" })
