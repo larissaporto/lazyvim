e334177b70bd2ac65bda3d7c6f0e337e26d14440
Larissa Silva <larissa.silva@xing.com>
2024-03-28 13:37:39 +0100

    Force Meslo font in neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.swapfile = false
+-- vim.g.neovide_remember_window_size = true
+vim.o.guifont = "MesloLGS Nerd Font"
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -14,4 +14,24 @@ return {
       require("better_escape").setup()
     end,
   },
+
+  -- {
+  --   "echasnovski/mini.move",
+  --   version = "*",
+  --   config = function()
+  --     require("mini.move").setup({
+  --       mappings = {
+  --         left = "<A-left>",
+  --         right = "<A-right>",
+  --         down = "<A-down>",
+  --         up = "<A-up>",
+  --
+  --         line_left = "<A-left>",
+  --         line_right = "<A-right>",
+  --         line_down = "<A-down>",
+  --         line_up = "<A-up>",
+  --       },
+  --     })
+  --   end,
+  -- },
 }
